60221ec009a285c282e5baaf4d32082bebaf0cd5
Bertrand LE GAL <bertrand.legal@ims-bordeaux.fr>
2025-06-24 11:57:42 +0200

    Update f_function.hpp

--- src/f_function/f_function.hpp
+++ src/f_function/f_function.hpp
@@ -15,19 +15,27 @@
 template <uint32_t gf_size>
 void f_function(symbols_t *dst, symbols_t *src_a, symbols_t *src_b)
 {
-
+    //
+    // Switch from time to frequency domain if needed
+    //
     if (src_a->is_freq == false)
     {
         fwht<gf_size>(src_a->value);
         src_a->is_freq = true;
     }
 
+    //
+    // Switch from time to frequency domain if needed
+    //  
     if (src_b->is_freq == false)
     {
         fwht<gf_size>(src_b->value);
         src_b->is_freq = true;
     }
 
+    //
+    // Element-wise multiplication of the two input symbols because we are in frequency domain !
+    //
     for (size_t i = 0; i < gf_size; i++)
     {
         dst->value[i] = src_a->value[i] * src_b->value[i];
@@ -35,18 +43,20 @@ void f_function(symbols_t *dst, symbols_t *src_a, symbols_t *src_b)
     }
     dst->is_freq = true; // a.a we do CN in FD
 
-    fwht<gf_size>(dst->value);
-    dst->is_freq = false;
+    // BLG : we decide to not normalize the CN output in frequency domain
+    
+//    fwht<gf_size>(dst->value);
+//    dst->is_freq = false;
 
-    value_type s1 = 0;
-    for (size_t i = 0; i < gf_size; i++)
-    {
-        s1 += dst->value[i];
-    }
-    for (size_t i = 0; i < gf_size; i++)
-    {
-        dst->value[i] /= s1;
-    }
+//    value_type s1 = 0;
+//    for (size_t i = 0; i < gf_size; i++)
+//    {
+//        s1 += dst->value[i];
+//    }
+//    for (size_t i = 0; i < gf_size; i++)
+//    {
+//        dst->value[i] /= s1;
+//    }
 }
 //
 //
