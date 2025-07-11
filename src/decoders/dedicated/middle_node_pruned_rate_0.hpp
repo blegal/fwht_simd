f6d3c9e9c961efdc4db6dd55f509ac458974bde4
Bertrand LE GAL <bertrand.legal@ims-bordeaux.fr>
2025-07-11 14:05:34 +0200

    Update middle_node_pruned_rate_0.hpp

--- src/decoders/dedicated/middle_node_pruned_rate_0.hpp
+++ src/decoders/dedicated/middle_node_pruned_rate_0.hpp
@@ -11,7 +11,7 @@
 //
 //
 //
-template <int gf_size> inline __attribute__((always_inline)) void middle_node_pruned_rate_0(
+template <int gf_size> void middle_node_pruned_rate_0(
     uint16_t* __restrict decoded,  // Decoded symbols are the final output of the decoder (done on the left)
     uint16_t* __restrict symbols,  // Symbols are the ones going from leafs to root (done on the left)
     int        size      // Size is the number of symbols (should be a power of 2)
