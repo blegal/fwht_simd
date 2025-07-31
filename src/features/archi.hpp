f082071d750280c3289a5562a22047a2f420ec47
Bertrand LE GAL <bertrand.legal@gmail.com>
2025-07-31 09:34:32 +0200

    Update archi.hpp

--- src/features/archi.hpp
+++ src/features/archi.hpp
@@ -9,14 +9,17 @@
 #include "fwht/fwht_norm.hpp"
 
 #if defined(__ARM_NEON__) || defined(__ARM_NEON)
-#include "fwht/fwht_neon.hpp"
-#include "fwht/fwht_norm_neon.hpp"
-//#include "fwht/fwht_norm_neon_v2.hpp"
+    #include "fwht/fwht_neon.hpp"
+    #include "fwht/fwht_norm_neon.hpp"
+    #include "fwht/fwht_norm_neon_v2.hpp"
 #endif
-
+#if defined(__AVX512F__)
+    #include "fwht/fwht_avx512.hpp"
+    #include "fwht/fwht_norm_avx512.hpp"
+#endif
 #if defined(__AVX2__)
-#include "fwht/fwht_avx2.hpp"
-#include "fwht/fwht_norm_avx2.hpp"
+    #include "fwht/fwht_avx2.hpp"
+    #include "fwht/fwht_norm_avx2.hpp"
 #endif
 
 #if defined(__ARM_NEON__) || defined(__ARM_NEON)
@@ -24,6 +27,9 @@
 //    #define FWHT_NORM lwht_norm_generic
     #define FWHT_NORM fwht_norm_neon
 //    #define FWHT_NORM lwht_norm_generic
+
+#elif defined(__AVX512F__)
+    #define FWHT_NORM fwht_norm_avx512
 #elif defined(__AVX2__)
     #define FWHT_NORM fwht_norm_avx2
 #else
