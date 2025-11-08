15b839d2bca921a78b3b1375bbb5271e24de6f93
Bertrand LE GAL <bertrand.legal@ims-bordeaux.fr>
2025-11-08 18:55:23 +0100

    Benchmarking scripts

--- run_optiplex.sh
+++ run_optiplex.sh
@@ -1,3 +1,5 @@
+# mono-core experiments
+
 python3 ../build_all_N.py  --decoder dec3 --platform optiplex_1c --cores 1 --time 60
 python3 ../build_all_N.py  --decoder dec4 --platform optiplex_1c --cores 1 --time 60
 python3 ../build_all_N.py  --decoder dec5 --platform optiplex_1c --cores 1 --time 60
@@ -9,3 +11,17 @@ python3 ../build_all_GF.py --decoder dec5 --platform optiplex_1c --cores 1 --tim
 python3 ../build_all_R.py  --decoder dec3 --platform optiplex_1c --cores 1 --time 60
 python3 ../build_all_R.py  --decoder dec4 --platform optiplex_1c --cores 1 --time 60
 python3 ../build_all_R.py  --decoder dec5 --platform optiplex_1c --cores 1 --time 60
+
+# 8 core expermients (8 high-perf. + 4 low-energy)
+
+python3 ../build_all_N.py  --decoder dec3 --platform optiplex_8c --cores 8 --time 60
+python3 ../build_all_N.py  --decoder dec4 --platform optiplex_8c --cores 8 --time 60
+python3 ../build_all_N.py  --decoder dec5 --platform optiplex_8c --cores 8 --time 60
+
+python3 ../build_all_GF.py --decoder dec3 --platform optiplex_8c --cores 8 --time 60
+python3 ../build_all_GF.py --decoder dec4 --platform optiplex_8c --cores 8 --time 60
+python3 ../build_all_GF.py --decoder dec5 --platform optiplex_8c --cores 8 --time 60
+
+python3 ../build_all_R.py  --decoder dec3 --platform optiplex_8c --cores 8 --time 60
+python3 ../build_all_R.py  --decoder dec4 --platform optiplex_8c --cores 8 --time 60
+python3 ../build_all_R.py  --decoder dec5 --platform optiplex_8c --cores 8 --time 60
