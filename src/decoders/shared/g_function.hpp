3162975defc0cde2acf7e64e37d0cc81cdccc550
Bertrand LE GAL <bertrand.legal@ims-bordeaux.fr>
2025-11-10 11:22:42 +0100

    Update g_function.hpp

--- src/decoders/shared/g_function.hpp
+++ src/decoders/shared/g_function.hpp
@@ -5,10 +5,12 @@
 #include "utilities/utility_functions.hpp"
 
 #ifdef ABD_OPTIM
-#if _GF_ == 16
-#include "hadamard/Hadamard_64.hpp"
+#if _GF_ == 8
+#include "hadamard/Hadamard_8.hpp"
+#elif _GF_ == 16
+#include "hadamard/Hadamard_16.hpp"
 #elif _GF_ == 32
-#include "hadamard/Hadamard_128.hpp"
+#include "hadamard/Hadamard_32.hpp"
 #elif _GF_ == 64
 #include "hadamard/Hadamard_64.hpp"
 #elif _GF_ == 128
@@ -19,9 +21,9 @@
 #include "hadamard/Hadamard_512.hpp"
 #elif _GF_ == 1024
 #include "hadamard/Hadamard_1024.hpp"
-#elif _GF_ == 1024
+#elif _GF_ == 2048
 #include "hadamard/Hadamard_2048.hpp"
-#elif _GF_ == 1024
+#elif _GF_ == 4096
 #include "hadamard/Hadamard_4096.hpp"
 #endif
 #endif
