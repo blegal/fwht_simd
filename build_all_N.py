6d7f3bc59181f9c8ba2ae247fc2eed27b76f5e5c
Camille Monière <camille.moniere@univ-ubs.fr>
2025-11-11 00:03:07 +0100

    enhance build_all_N.py, retrocompatible

--- build_all_N.py
+++ build_all_N.py
@@ -2,11 +2,11 @@ import argparse
 import os
 import subprocess
 import sys
-#
-#
-#
-#
-#
+import random
+from pathlib import Path
+from typing import List
+
+
 def generate_config_header(N, GF):
     header_content = f"""#ifndef CONFIG_CODE_H
 #define CONFIG_CODE_H
@@ -15,90 +15,136 @@ def generate_config_header(N, GF):
 """
     with open("../src/definitions/code.hpp", "w") as f:
         f.write(header_content)
-#
-#
-#
-#
-#
+
+
 def compile_project():
-    log_file = "/tmp/process.log"
+    rdid = random.randint(0, 65536)
+    log_file = Path(f"/tmp/process_{rdid}.log")
+    while log_file.exists():
+        rdid = random.randint(0, 65536)
+        log_file = Path(f"/tmp/process_{rdid}.log")
     with open(log_file, "w") as f:
         print("🛠️  Compilation initiale (1/2)...")
-        result = subprocess.run(["make","clean"], stdout=f, stderr=subprocess.STDOUT)
+        result = subprocess.run(["make", "clean"], stdout=f, stderr=subprocess.STDOUT)
         if result.returncode != 0:
-            print(f"❌ Erreur: échec de la compilation (code retour {result.returncode}).")
+            print(
+                f"❌ Erreur: échec de la compilation (code retour {result.returncode})."
+            )
             sys.exit(1)
-        result = subprocess.run(["make","code_generator"], stdout=f, stderr=subprocess.STDOUT)
+        result = subprocess.run(
+            ["make", "code_generator"], stdout=f, stderr=subprocess.STDOUT
+        )
         if result.returncode != 0:
-            print(f"❌ Erreur: échec de la compilation (code retour {result.returncode}).")
+            print(
+                f"❌ Erreur: échec de la compilation (code retour {result.returncode})."
+            )
             sys.exit(1)
-        print(f"🚀 Generation du décodeur dédié...")
-        result = subprocess.run(["./code_generator", "--code-rate", "0.50", "--no-verbose"], stdout=f, stderr=subprocess.STDOUT)
+        print("🚀 Generation du décodeur dédié...")
+        result = subprocess.run(
+            ["./code_generator", "--code-rate", "0.50", "--no-verbose"],
+            stdout=f,
+            stderr=subprocess.STDOUT,
+        )
         if result.returncode != 0:
-            print(f"❌ Erreur: échec de la generation (code retour {result.returncode}).")
+            print(
+                f"❌ Erreur: échec de la generation (code retour {result.returncode})."
+            )
             sys.exit(1)
         print("🛠️  Compilation finale (2/2)...")
-        result = subprocess.run(["make","benchmarking"], stdout=f, stderr=subprocess.STDOUT)
+        result = subprocess.run(
+            ["make", "benchmarking"], stdout=f, stderr=subprocess.STDOUT
+        )
         if result.returncode != 0:
-            print(f"❌ Erreur: échec de la compilation (code retour {result.returncode}).")
+            print(
+                f"❌ Erreur: échec de la compilation (code retour {result.returncode})."
+            )
             sys.exit(1)
-#
-#
-#
-#
-#
+        os.remove(log_file)
+
+
 def run_executable(N, GF, decoder, platform, cores, time, log_dir):
     executable = "./benchmarking"
-    log_file = os.path.join(log_dir, f"{decoder}_N{N}_GF{GF}_{platform}.log")
-    cmd = [executable, "--decoder", decoder, "--no-color", "--cores" , cores, "--time", time, "--code-rate", "0.50"]
+    log_file = os.path.join(log_dir, f"{decoder}_N{N}_GF{GF}_{platform}_{cores}c.log")
+    cmd = [
+        executable,
+        "--decoder",
+        decoder,
+        "--no-color",
+        "--cores",
+        f"{cores}",
+        "--time",
+        f"{time}",
+        "--code-rate",
+        "0.50",
+    ]
 
     print(f"🚀 Exécution: {cmd} pour N={N} et GF={GF}")
 
     with open(log_file, "w") as f:
         result = subprocess.run(cmd, stdout=f, stderr=subprocess.STDOUT)
         if result.returncode != 0:
-            print(f"❌ Erreur: échec de l'exécution pour N={N} (code retour {result.returncode}).")
+            print(
+                f"❌ Erreur: échec de l'exécution pour N={N} (code retour {result.returncode})."
+            )
             sys.exit(1)
-#
-#
-#
-#
-#
-def generate_report(log_dir, decoder, platform, GF, Ns):
-    report_file = os.path.join(log_dir, f"N_{decoder}_{platform}.txt")
-    with open(report_file, "w") as report:
-        header = "   N    K    R   GF   Coded    Info   Lat"
-        report.write(header + "\n")
+
+def generate_report(Ns: List[int], GF, decoder, platform, cores, duration, log_dir):
+    report_file: Path = Path(log_dir).joinpath(f"N_{decoder}_{platform}.txt")
+    first_time: bool = not report_file.exists()
+    with open(report_file, "a") as report:
+        if first_time:
+            header = "   N    K    R   GF   Coded    Info   Lat Threads Duration"
+            report.write(header + "\n")
 
         for N in Ns:
-            log_file = os.path.join(log_dir, f"{decoder}_N{N}_GF{GF}_{platform}.log")
+            log_file = os.path.join(
+                log_dir, f"{decoder}_N{N}_GF{GF}_{platform}_{cores}c.log"
+            )
             if os.path.isfile(log_file):
                 with open(log_file, "r") as f:
                     lines = f.readlines()
                     if lines:
-                        last_line = lines[-1]#.strip()
-                        report.write(f"{last_line}")#\n")
+                        last_line_list = lines[-1].strip().split(" ")
+                        first_value = int(last_line_list[0])
+                        remains = " ".join(last_line_list[1:])
+                        report.write(f"{N:4d} {remains} {cores:7d} {duration:8d}\n")
                     else:
                         report.write(f"{GF} MISSING_DATA\n")
             else:
                 report.write(f"{N} MISSING_LOG\n")
 
     print(f"📄 Rapport généré: {report_file}")
-#
-#
-#
-#
-#
+
 def main():
-    parser = argparse.ArgumentParser(description="Compile, exécute et génère un rapport de benchmarking.")
-    parser.add_argument("--decoder",  required=True, choices=["dec1", "dec2", "dec3", "dec4", "dec5"], help="Nom du décodeur (ex: dec1)")
-    parser.add_argument("--platform", required=True, help="Nom de la plateforme pour le nommage du log")
-    parser.add_argument("--cores",    required=True, help="Nombre de coeurs actifs")
-    parser.add_argument("--time",     required=True, help="Temps de run pour la mesure")
+    parser = argparse.ArgumentParser(
+        description="Compile, exécute et génère un rapport de benchmarking."
+    )
+    parser.add_argument(
+        "--decoder",
+        required=True,
+        choices=["dec1", "dec2", "dec3", "dec4", "dec5"],
+        help="Nom du décodeur (ex: dec1)",
+    )
+    parser.add_argument(
+        "--platform", required=True, help="Nom de la plateforme pour le nommage du log"
+    )
+    parser.add_argument(
+        "--cores", type=int, required=True, help="Nombre de coeurs actifs"
+    )
+    parser.add_argument(
+        "--time", type=int, required=True, help="Temps de run pour la mesure"
+    )
+    parser.add_argument(
+        "--Ns",
+        nargs="*",
+        type=int,
+        default="8 16 32 64 128 256 512 1024",
+        help="Valeur de N à tester",
+    )
     args = parser.parse_args()
 
     GF = 64  # fixe ou tu peux le rendre paramétrable
-    Ns = [8, 16, 32, 64, 128, 256, 512, 1024]
+    Ns = args.Ns
 
     log_dir = "log"
     os.makedirs(log_dir, exist_ok=True)
@@ -109,19 +155,13 @@ def main():
 
         compile_project()
 
-        run_executable(N, GF, args.decoder, args.platform, args.cores, args.time, log_dir)
+        run_executable(
+            N, GF, args.decoder, args.platform, int(args.cores), int(args.time), log_dir
+        )
 
-    generate_report(log_dir, args.decoder, args.platform, GF, Ns)
+    generate_report(Ns, GF, args.decoder, args.platform, args.cores, args.time, log_dir)
     print("✅ Tout est terminé.")
-#
-#
-#
-#
-#
+
+
 if __name__ == "__main__":
     main()
-#
-#
-#
-#
-#
