995365d20bd9dd6738b8dcb7f9cac188c9ac150b
Bertrand LE GAL <blegal@users.noreply.github.com>
2025-11-11 16:32:48 +0100

    Update decoder_naive_pruning.cpp

--- src/decoders/naive_pruning/decoder_naive_pruning.cpp
+++ src/decoders/naive_pruning/decoder_naive_pruning.cpp
@@ -318,7 +318,7 @@ void decoder_naive_pruning<gf_size>::middle_node_with_pruning(
         //
         //
         int check_node = 0;
-        uint16_t arg_1[256];
+        uint16_t arg_1[512];
         for (int i = 0; i < size; i++) {
             int value              = argmax<gf_size>(inputs[i].value);
             check_node            ^= value;
@@ -333,7 +333,7 @@ void decoder_naive_pruning<gf_size>::middle_node_with_pruning(
             return;
         }
 
-        uint16_t arg_2[256];
+        uint16_t arg_2[512];
         for (int j = 0; j < size; j++) {
             arg_2[j] = argmax2<gf_size>(inputs[j].value, arg_1[j]);
         }
