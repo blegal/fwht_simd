638ff3362893815edd5fe9dd65a4d7ead860a5c1
Bertrand LE GAL <bertrand.legal@ims-bordeaux.fr>
2025-12-09 17:42:51 +0100

    Macbook Pro (N)

--- scripts/run_apple.sh
+++ scripts/run_apple.sh
@@ -2,10 +2,12 @@ rm -rf ./log
 
 # Amélioration de notre algo de décodage
 
+python3 ../build_all_N.py  --decoder dec1 --platform macOs_1c --cores 1  --time 30
 python3 ../build_all_N.py  --decoder dec3 --platform macOs_1c --cores 1  --time 30
 python3 ../build_all_N.py  --decoder dec4 --platform macOs_1c --cores 1  --time 30
 python3 ../build_all_N.py  --decoder dec5 --platform macOs_1c --cores 1  --time 30
 
+python3 ../build_all_GF.py --decoder dec1 --platform macOs_1c --cores 1  --time 30
 python3 ../build_all_GF.py --decoder dec3 --platform macOs_1c --cores 1  --time 30
 python3 ../build_all_GF.py --decoder dec4 --platform macOs_1c --cores 1  --time 30
 python3 ../build_all_GF.py --decoder dec5 --platform macOs_1c --cores 1  --time 30
@@ -16,11 +18,14 @@ python3 ../build_all_R.py  --decoder dec5 --platform macOs_1c --cores 1  --time 
 
 # ABC
 
+python3 ../build_all_N.py  --decoder dec3 --platform macOs_8c --cores 8  --time 60
 python3 ../build_all_N.py  --decoder dec4 --platform macOs_8c --cores 8  --time 60
 python3 ../build_all_N.py  --decoder dec5 --platform macOs_8c --cores 8  --time 60
 
+python3 ../build_all_GF.py --decoder dec3 --platform macOs_8c --cores 8  --time 60
 python3 ../build_all_GF.py --decoder dec4 --platform macOs_8c --cores 8  --time 60
 python3 ../build_all_GF.py --decoder dec5 --platform macOs_8c --cores 8  --time 60
 
+python3 ../build_all_R.py  --decoder dec3 --platform macOs_8c --cores 8  --time 60
 python3 ../build_all_R.py  --decoder dec4 --platform macOs_8c --cores 8  --time 60
 python3 ../build_all_R.py  --decoder dec5 --platform macOs_8c --cores 8  --time 60
