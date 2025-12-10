a2b042b2197cfa1dd0934fc08746407e18ebdf94
Bertrand LE GAL <bertrand.legal@gmail.com>
2025-12-10 15:44:45 +0100

    Update middle_node_pruned_rep_after_g.hpp

--- src/decoders/dedicated/middle_node_pruned_rep_after_g.hpp
+++ src/decoders/dedicated/middle_node_pruned_rep_after_g.hpp
@@ -19,9 +19,6 @@ template <int gf_size> void middle_node_pruned_rep_after_g(
     uint16_t*  __restrict symbols, // Symbols are the ones going from leafs to root (done on the left)
     const int size                 // Size is the number of symbols (should be a power of 2)
 ) {
-        // ON PEUT REUTILISER LE SYMBOL n°0 POUR LES CALCULS
-        // + NORMALISATION A LA VOLEE
-#if 1 // optimization
         float temp[gf_size];
         for (int j = 0; j < gf_size; j++)
             temp[j] = inputs[0].value[j] * inputs[1].value[j];
@@ -32,23 +29,6 @@ template <int gf_size> void middle_node_pruned_rep_after_g(
             for (int j = 0; j < gf_size; j++)
                 temp[j] *= inputs[i].value[j] ;
         }
-#else
-    float temp[gf_size];
-    for (int j = 0; j < gf_size; j++)
-        temp[j] = inputs[0].value[j] * inputs[1].value[j];
-
-    for(int i = 2; i < size; i+=2){
-        float sum = 0.f;
-        for (int j = 0; j < gf_size; j++) {
-            const float val = temp[j] * inputs[i].value[j];
-            temp[j] = val;
-            sum    += val;
-        }
-        const float inv = 1.f / sum;
-        for (int j = 0; j < gf_size; j++)
-            temp[j] *= inputs[i+1].value[j] * inv;
-    }
-#endif
         const int value = argmax<gf_size>( temp );
 
         for(int i = 0; i < size; i++)
