af9d4ebbcaf7ff6157214e0fdfab1697d9291dd6
Bertrand LE GAL <bertrand.legal@ims-bordeaux.fr>
2025-12-19 10:53:33 +0100

    Result on macOs (N)

--- scripts/run_apple.sh
+++ scripts/run_apple.sh
@@ -14,21 +14,21 @@ rm -rf ./log
 #python3 ../build_all_GF.py --decoder dec4 --platform macOs_1c --cores 1  --time 30
 #python3 ../build_all_GF.py --decoder dec5 --platform macOs_1c --cores 1  --time 30
 
-python3 ../build_all_R.py  --decoder dec0 --platform macOs_1c --cores 1  --time 30
-python3 ../build_all_R.py  --decoder dec1 --platform macOs_1c --cores 1  --time 30
-python3 ../build_all_R.py  --decoder dec3 --platform macOs_1c --cores 1  --time 60
+#python3 ../build_all_R.py  --decoder dec0 --platform macOs_1c --cores 1  --time 30
+#python3 ../build_all_R.py  --decoder dec1 --platform macOs_1c --cores 1  --time 30
+python3 ../build_all_R.py  --decoder dec3 --platform macOs_1c --cores 1  --time 30
 python3 ../build_all_R.py  --decoder dec4 --platform macOs_1c --cores 1  --time 30
 python3 ../build_all_R.py  --decoder dec5 --platform macOs_1c --cores 1  --time 30
 
 # ABC
 
-python3 ../build_all_N.py  --decoder dec3 --platform macOs_8c --cores 8  --time 60
-python3 ../build_all_N.py  --decoder dec4 --platform macOs_8c --cores 8  --time 60
-python3 ../build_all_N.py  --decoder dec5 --platform macOs_8c --cores 8  --time 60
+#python3 ../build_all_N.py  --decoder dec3 --platform macOs_8c --cores 8  --time 60
+#python3 ../build_all_N.py  --decoder dec4 --platform macOs_8c --cores 8  --time 60
+#python3 ../build_all_N.py  --decoder dec5 --platform macOs_8c --cores 8  --time 60
 
-python3 ../build_all_GF.py --decoder dec3 --platform macOs_8c --cores 8  --time 60
-python3 ../build_all_GF.py --decoder dec4 --platform macOs_8c --cores 8  --time 60
-python3 ../build_all_GF.py --decoder dec5 --platform macOs_8c --cores 8  --time 60
+#python3 ../build_all_GF.py --decoder dec3 --platform macOs_8c --cores 8  --time 60
+#python3 ../build_all_GF.py --decoder dec4 --platform macOs_8c --cores 8  --time 60
+#python3 ../build_all_GF.py --decoder dec5 --platform macOs_8c --cores 8  --time 60
 
 python3 ../build_all_R.py  --decoder dec3 --platform macOs_8c --cores 8  --time 60
 python3 ../build_all_R.py  --decoder dec4 --platform macOs_8c --cores 8  --time 60
