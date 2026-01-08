b67d3f23fd5dc111b5de20996a7559d9a8ca5a98
Bertrand LE GAL <blegal@users.noreply.github.com>
2026-01-08 14:35:12 +0000

    New PI5

--- scripts/run_pi5.sh
+++ scripts/run_pi5.sh
@@ -12,22 +12,22 @@ python3 ../build_all_GF.py --decoder dec3 --platform pi5_1c --cores 1 --time 60
 python3 ../build_all_GF.py --decoder dec4 --platform pi5_1c --cores 1 --time 60
 python3 ../build_all_GF.py --decoder dec5 --platform pi5_1c --cores 1 --time 60
 
-python3 ../build_all_R.py  --decoder dec0 --platform pi5_1c --cores 1 --time 60
-python3 ../build_all_R.py  --decoder dec1 --platform pi5_1c --cores 1 --time 60
+#python3 ../build_all_R.py  --decoder dec0 --platform pi5_1c --cores 1 --time 60
+#python3 ../build_all_R.py  --decoder dec1 --platform pi5_1c --cores 1 --time 60
 python3 ../build_all_R.py  --decoder dec3 --platform pi5_1c --cores 1 --time 60
 python3 ../build_all_R.py  --decoder dec4 --platform pi5_1c --cores 1 --time 60
 python3 ../build_all_R.py  --decoder dec5 --platform pi5_1c --cores 1 --time 60
 
 # 4 cores experiments
 
-python3 ../build_all_N.py  --decoder dec3 --platform pi5_4c --cores 1 --time 60
-python3 ../build_all_N.py  --decoder dec4 --platform pi5_4c --cores 1 --time 60
-python3 ../build_all_N.py  --decoder dec5 --platform pi5_4c --cores 1 --time 60
+python3 ../build_all_N.py  --decoder dec3 --platform pi5_4c --cores 4 --time 60
+python3 ../build_all_N.py  --decoder dec4 --platform pi5_4c --cores 4 --time 60
+python3 ../build_all_N.py  --decoder dec5 --platform pi5_4c --cores 4 --time 60
 
-python3 ../build_all_GF.py --decoder dec3 --platform pi5_4c --cores 1 --time 60
-python3 ../build_all_GF.py --decoder dec4 --platform pi5_4c --cores 1 --time 60
-python3 ../build_all_GF.py --decoder dec5 --platform pi5_4c --cores 1 --time 60
+python3 ../build_all_GF.py --decoder dec3 --platform pi5_4c --cores 4 --time 60
+python3 ../build_all_GF.py --decoder dec4 --platform pi5_4c --cores 4 --time 60
+python3 ../build_all_GF.py --decoder dec5 --platform pi5_4c --cores 4 --time 60
 
-python3 ../build_all_R.py  --decoder dec3 --platform pi5_4c --cores 1 --time 60
-python3 ../build_all_R.py  --decoder dec4 --platform pi5_4c --cores 1 --time 60
-python3 ../build_all_R.py  --decoder dec5 --platform pi5_4c --cores 1 --time 60
+python3 ../build_all_R.py  --decoder dec3 --platform pi5_4c --cores 4 --time 60
+python3 ../build_all_R.py  --decoder dec4 --platform pi5_4c --cores 4 --time 60
+python3 ../build_all_R.py  --decoder dec5 --platform pi5_4c --cores 4 --time 60
