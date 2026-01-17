0ce6bad18ee73da886c5398e5827fc3ccf008ace
Bertrand LE GAL <blegal@users.noreply.github.com>
2026-01-17 13:53:02 +0100

    Update hls_decoder.cpp

--- src/decoders/hls/hls_decoder.cpp
+++ src/decoders/hls/hls_decoder.cpp
@@ -7,8 +7,8 @@
 //
 //
 #include <cstdint>
-//#include "ap_fixed.h"
-#include "include/ap_fixed.h"
+#include "ap_fixed.h"
+//#include "include/ap_fixed.h"
 
 #define gf_size 64
 
@@ -156,7 +156,7 @@ uint8_t vec_i_unroll_argmax(const t_int18b inp)
 t_int18b vec_i_norm(const t_int48b src)
 {
 #pragma HLS INLINE
-#pragma HLS PIPELINE
+//#pragma HLS PIPELINE
 #pragma HLS ARRAY_PARTITION dim=1 type=complete variable=src.value
 
 	int48b absv[64];
@@ -167,79 +167,77 @@ t_int18b vec_i_norm(const t_int48b src)
     	else			absv[i] =  value;
     }
 
+    //
+    // On calcule le premier facteur de scaling basé sur le maximum
+    //
     int48b loc[4];
-    // Traitement de 16 données par ligne
 #pragma HLS ARRAY_PARTITION dim=1 type=complete variable=loc
-    loc[0] = ((absv[ 0] + absv[ 1]) + (absv[ 2] + absv[ 3])) +
-             ((absv[ 4] + absv[ 5]) + (absv[ 6] + absv[ 7])) +
-             ((absv[ 8] + absv[ 9]) + (absv[10] + absv[11])) +
-             ((absv[12] + absv[13]) + (absv[14] + absv[15]));
+    loc[0] = ((absv[ 0] + absv[ 1]) + (absv[ 2] + absv[ 3])) + ((absv[ 4] + absv[ 5]) + (absv[ 6] + absv[ 7])) +
+             ((absv[ 8] + absv[ 9]) + (absv[10] + absv[11])) + ((absv[12] + absv[13]) + (absv[14] + absv[15]));
 
-    loc[1] = ((absv[16] + absv[17]) + (absv[18] + absv[19])) +
-             ((absv[20] + absv[21]) + (absv[22] + absv[23])) +
-             ((absv[24] + absv[25]) + (absv[26] + absv[27])) +
-             ((absv[28] + absv[29]) + (absv[30] + absv[31]));
+    loc[1] = ((absv[16] + absv[17]) + (absv[18] + absv[19])) + ((absv[20] + absv[21]) + (absv[22] + absv[23])) +
+             ((absv[24] + absv[25]) + (absv[26] + absv[27])) + ((absv[28] + absv[29]) + (absv[30] + absv[31]));
 
-    loc[2] = ((absv[32] + absv[33]) + (absv[34] + absv[35])) +
-             ((absv[36] + absv[37]) + (absv[38] + absv[39])) +
-             ((absv[40] + absv[41]) + (absv[42] + absv[43])) +
-             ((absv[44] + absv[45]) + (absv[46] + absv[47]));
+    loc[2] = ((absv[32] + absv[33]) + (absv[34] + absv[35])) + ((absv[36] + absv[37]) + (absv[38] + absv[39])) +
+             ((absv[40] + absv[41]) + (absv[42] + absv[43])) + ((absv[44] + absv[45]) + (absv[46] + absv[47]));
 
-    loc[3] = ((absv[48] + absv[49]) + (absv[50] + absv[51])) +
-             ((absv[52] + absv[53]) + (absv[54] + absv[55])) +
-             ((absv[56] + absv[57]) + (absv[58] + absv[59])) +
-             ((absv[60] + absv[61]) + (absv[62] + absv[63]));
-
+    loc[3] = ((absv[48] + absv[49]) + (absv[50] + absv[51])) + ((absv[52] + absv[53]) + (absv[54] + absv[55])) +
+             ((absv[56] + absv[57]) + (absv[58] + absv[59])) + ((absv[60] + absv[61]) + (absv[62] + absv[63]));
     const int48b sum = loc[0] + loc[1] + loc[2] + loc[3];
 
+    //
+    // On calcule le premier facteur de scaling basé sur la position du MSB
+    //
+
+
     ap_uint<6> factor;
-    if      (sum[i_W-1]  == 1) factor = 0;
-    else if (sum[i_W-2]  == 1) factor = 1;
-    else if (sum[i_W-3]  == 1) factor = 2;
-    else if (sum[i_W-4]  == 1) factor = 3;
-    else if (sum[i_W-5]  == 1) factor = 4;
-    else if (sum[i_W-6]  == 1) factor = 5;
-    else if (sum[i_W-7]  == 1) factor = 6;
-    else if (sum[i_W-8]  == 1) factor = 7;
-    else if (sum[i_W-9]  == 1) factor = 8;
-    else if (sum[i_W-10] == 1) factor = 9;
-    else if (sum[i_W-11] == 1) factor = 10;
-    else if (sum[i_W-12] == 1) factor = 11;
-    else if (sum[i_W-13] == 1) factor = 12;
-    else if (sum[i_W-14] == 1) factor = 13;
-    else if (sum[i_W-15] == 1) factor = 14;
-    else if (sum[i_W-16] == 1) factor = 15;
-    else if (sum[i_W-17] == 1) factor = 16;
-    else if (sum[i_W-18] == 1) factor = 17;
-    else if (sum[i_W-19] == 1) factor = 18;
-    else if (sum[i_W-20] == 1) factor = 19;
-    else if (sum[i_W-21] == 1) factor = 20;
-    else if (sum[i_W-22] == 1) factor = 21;
-    else if (sum[i_W-23] == 1) factor = 22;
-    else if (sum[i_W-24] == 1) factor = 23;
-    else if (sum[i_W-25] == 1) factor = 24;
-    else if (sum[i_W-26] == 1) factor = 25;
-    else if (sum[i_W-27] == 1) factor = 26;
-    else if (sum[i_W-28] == 1) factor = 27;
-    else if (sum[i_W-29] == 1) factor = 28;
-    else if (sum[i_W-30] == 1) factor = 29;
-    else if (sum[i_W-31] == 1) factor = 30;
-    else if (sum[i_W-32] == 1) factor = 31;
-    else if (sum[i_W-33] == 1) factor = 32;
-    else if (sum[i_W-34] == 1) factor = 33;
-    else if (sum[i_W-35] == 1) factor = 34;
-    else if (sum[i_W-36] == 1) factor = 35;
-    else if (sum[i_W-37] == 1) factor = 36;
-    else if (sum[i_W-38] == 1) factor = 37;
-    else if (sum[i_W-39] == 1) factor = 38;
-    else if (sum[i_W-40] == 1) factor = 39;
-    else if (sum[i_W-41] == 1) factor = 40;
-    else if (sum[i_W-42] == 1) factor = 41;
-    else if (sum[i_W-43] == 1) factor = 42;
-    else if (sum[i_W-44] == 1) factor = 43;
-    else if (sum[i_W-45] == 1) factor = 44;
-    else if (sum[i_W-46] == 1) factor = 45;
-    else if (sum[i_W-47] == 1) factor = 46;
+    if      (sum[i_norm_width- 1] == 1) factor = 0;
+    else if (sum[i_norm_width- 2] == 1) factor = 1;
+    else if (sum[i_norm_width- 3] == 1) factor = 2;
+    else if (sum[i_norm_width- 4] == 1) factor = 3;
+    else if (sum[i_norm_width- 5] == 1) factor = 4;
+    else if (sum[i_norm_width- 6] == 1) factor = 5;
+    else if (sum[i_norm_width- 7] == 1) factor = 6;
+    else if (sum[i_norm_width- 8] == 1) factor = 7;
+    else if (sum[i_norm_width- 9] == 1) factor = 8;
+    else if (sum[i_norm_width-10] == 1) factor = 9;
+    else if (sum[i_norm_width-11] == 1) factor = 10;
+    else if (sum[i_norm_width-12] == 1) factor = 11;
+    else if (sum[i_norm_width-13] == 1) factor = 12;
+    else if (sum[i_norm_width-14] == 1) factor = 13;
+    else if (sum[i_norm_width-15] == 1) factor = 14;
+    else if (sum[i_norm_width-16] == 1) factor = 15;
+    else if (sum[i_norm_width-17] == 1) factor = 16;
+    else if (sum[i_norm_width-18] == 1) factor = 17;
+    else if (sum[i_norm_width-19] == 1) factor = 18;
+    else if (sum[i_norm_width-20] == 1) factor = 19;
+    else if (sum[i_norm_width-21] == 1) factor = 20;
+    else if (sum[i_norm_width-22] == 1) factor = 21;
+    else if (sum[i_norm_width-23] == 1) factor = 22;
+    else if (sum[i_norm_width-24] == 1) factor = 23;
+    else if (sum[i_norm_width-25] == 1) factor = 24;
+    else if (sum[i_norm_width-26] == 1) factor = 25;
+    else if (sum[i_norm_width-27] == 1) factor = 26;
+    else if (sum[i_norm_width-28] == 1) factor = 27;
+    else if (sum[i_norm_width-29] == 1) factor = 28;
+    else if (sum[i_norm_width-30] == 1) factor = 29;
+    else if (sum[i_norm_width-31] == 1) factor = 30;
+    else if (sum[i_norm_width-32] == 1) factor = 31;
+    else if (sum[i_norm_width-33] == 1) factor = 32;
+    else if (sum[i_norm_width-34] == 1) factor = 33;
+    else if (sum[i_norm_width-35] == 1) factor = 34;
+    else if (sum[i_norm_width-36] == 1) factor = 35;
+    else if (sum[i_norm_width-37] == 1) factor = 36;
+    else if (sum[i_norm_width-38] == 1) factor = 37;
+    else if (sum[i_norm_width-39] == 1) factor = 38;
+    else if (sum[i_norm_width-40] == 1) factor = 39;
+    else if (sum[i_norm_width-41] == 1) factor = 40;
+    else if (sum[i_norm_width-42] == 1) factor = 41;
+    else if (sum[i_norm_width-43] == 1) factor = 42;
+    else if (sum[i_norm_width-44] == 1) factor = 43;
+    else if (sum[i_norm_width-45] == 1) factor = 44;
+    else if (sum[i_norm_width-46] == 1) factor = 45;
+    else if (sum[i_norm_width-47] == 1) factor = 46;
     else factor = 47;
 
     t_int18b dst;
@@ -260,7 +258,7 @@ t_int18b vec_i_norm(const t_int48b src)
 t_int24b extend(const t_int18b& src)
 {
 #pragma HLS INLINE
-#pragma HLS PIPELINE
+//#pragma HLS PIPELINE
 #pragma HLS ARRAY_PARTITION dim=1 type=complete variable=src.value
 	t_int24b dst;
 #pragma HLS ARRAY_PARTITION dim=1 type=complete variable=dst.value
@@ -279,7 +277,7 @@ t_int24b extend(const t_int18b& src)
 t_int24b fwht(const t_int18b src)
 {
 #pragma HLS INLINE
-#pragma HLS PIPELINE
+//#pragma HLS PIPELINE
 #pragma HLS ARRAY_PARTITION dim=1 type=complete variable=src.value
     /*
      * ========= ÉTAGE 1 (distance 32) =========
@@ -346,7 +344,7 @@ t_int24b fwht(const t_int18b src)
 t_int48b vec_i_mul_f(const t_int24b src_1, const t_int24b src_2)
 {
 #pragma HLS INLINE
-#pragma HLS PIPELINE
+//#pragma HLS PIPELINE
 #pragma HLS ARRAY_PARTITION dim=1 type=complete variable=src_1.value
 #pragma HLS ARRAY_PARTITION dim=1 type=complete variable=src_2.value
 	t_int48b dst;
@@ -367,7 +365,7 @@ t_int48b vec_i_mul_f(const t_int24b src_1, const t_int24b src_2)
 t_int48b vec_i_mul_g(const t_int24b src_1, const t_int24b src_2, const uint8_t symbol)
 {
 #pragma HLS INLINE
-#pragma HLS PIPELINE
+//#pragma HLS PIPELINE
 #pragma HLS ARRAY_PARTITION dim=1 type=complete variable=src_1.value
 #pragma HLS ARRAY_PARTITION dim=1 type=complete variable=src_2.value
 
@@ -409,7 +407,7 @@ t_int18b datapath(const t_int18b lwht_in_a, const t_int18b lwht_in_b, const uint
 	const auto mult_in_e   = en_lwth ? mult_in_a : mult_in_c;
 	const auto mult_in_f   = en_lwth ? mult_in_b : mult_in_d;
 
-	const auto norm_in_a   = vec_i_mul_g(mult_in_e, mult_in_f, 0); // f_mode
+	const auto norm_in_a   = vec_i_mul_g(mult_in_e, mult_in_f, symbol); // f_mode
 	const auto memo_in_a   = vec_i_norm( norm_in_a );
 
 #pragma HLS ARRAY_PARTITION dim=1 type=complete variable=mult_in_a.value
