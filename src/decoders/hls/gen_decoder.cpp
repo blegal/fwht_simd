0a19601bea3255607e9bdd890879d6a38e0a3b11
Bertrand LE GAL <bertrand.legal@gmail.com>
2026-01-22 12:26:13 +0100

    Create gen_decoder.cpp

---
+++ src/decoders/hls/gen_decoder.cpp
@@ -0,0 +1,605 @@
+#include "types.hpp"
+#include "f_argmax.hpp"
+#include "f_extend.hpp"
+#include "f_fwht.hpp"
+#include "f_mult.hpp"
+#include "f_norm.hpp"
+#include "f_datapath.hpp"
+#include "f_decision.hpp"
+//
+//
+//
+//////////////////////////////////////////////////////////////////////
+//
+//
+//
+#define N            256
+#define gf_size      64
+#define log2_gf_size  6
+#define data_width   18
+//
+//
+//
+//////////////////////////////////////////////////////////////////////
+//
+//
+//
+void the_decoder_v2(
+			t_i_memo channel[N],
+			uint16_t decoded[N])
+{
+
+  t_i_memo internal[N];
+#pragma HLS ARRAY_PARTITION dim=2 type=complete variable=internal
+
+  uint8_t  symbols [N];
+#pragma HLS ARRAY_PARTITION dim=1 type=complete variable=symbols
+
+  t_i_memo lwht_in_a, lwht_in_b;
+#pragma HLS ARRAY_PARTITION dim=1 type=complete variable=lwht_in_a.value
+#pragma HLS ARRAY_PARTITION dim=1 type=complete variable=lwht_in_b.value
+
+  t_i_memo memo_in_a;
+#pragma HLS ARRAY_PARTITION dim=1 type=complete variable=memo_in_a.value
+
+  uint8_t symbol_v;
+  uint8_t symbol_x;
+
+  // f_function_proba_in
+  loop_1 : for (int s = 0; s < 128; s++) {
+#pragma HLS PIPELINE
+    lwht_in_a   = channel[s    ];
+    lwht_in_b   = channel[s + 128];
+    memo_in_a   = datapath(lwht_in_a, lwht_in_b, true, 0);
+    internal[s] = memo_in_a;
+  }
+
+  // f_function_freq_in
+  loop_2 : for (int s = 0; s < 64; s++) {
+#pragma HLS PIPELINE
+    lwht_in_a   = internal[s + 0];
+    lwht_in_b   = internal[s + 64];
+    memo_in_a   = datapath(lwht_in_a, lwht_in_b, false, 0);
+    internal[s + 128] = memo_in_a;
+  }
+
+  // f_function_freq_in
+  loop_3 : for (int s = 0; s < 32; s++) {
+#pragma HLS PIPELINE
+    lwht_in_a   = internal[s + 128];
+    lwht_in_b   = internal[s + 160];
+    memo_in_a   = datapath(lwht_in_a, lwht_in_b, false, 0);
+    internal[s + 192] = memo_in_a;
+  }
+
+  // f_function_freq_in<64>(....); NO F COMPUTATIONS AS WE HAVE A RATE 0 NODE AFTER !
+  // middle_node_pruned_rate_0
+  loop_4 : for (int s = 0; s < 16; s++) {
+#pragma HLS PIPELINE
+    symbols[s + 0] = 0;
+    decoded[s + 0] = 0;
+  }
+
+  // g_function_freq_in_after_rate_0
+  loop_5 : for (int s = 0; s < 16; s++) {
+#pragma HLS PIPELINE
+    lwht_in_a   = internal[s + 192];
+    lwht_in_b   = internal[s + 208];
+    memo_in_a   = datapath(lwht_in_a, lwht_in_b, true, 0);
+    internal[s + 224] = memo_in_a;
+  }
+
+  // f_function_freq_in<64>(....); NO F COMPUTATIONS AS WE HAVE A RATE 0 NODE AFTER !
+  // middle_node_pruned_rate_0
+  loop_6 : for (int s = 0; s < 8; s++) {
+#pragma HLS PIPELINE
+    symbols[s + 16] = 0;
+    decoded[s + 16] = 0;
+  }
+
+  // g_function_proba_in_after_rate_0
+  loop_7 : for (int s = 0; s < 8; s++) {
+#pragma HLS PIPELINE
+    lwht_in_a   = internal[s + 224];
+    lwht_in_b   = internal[s + 232];
+    memo_in_a   = datapath(lwht_in_a, lwht_in_b, false, 0);
+    internal[s + 240] = memo_in_a;
+  }
+
+  // f_function_freq_in<64>(....); NO F COMPUTATIONS AS WE HAVE A RATE 0 NODE AFTER !
+  // middle_node_pruned_rate_0
+  loop_8 : for (int s = 0; s < 4; s++) {
+#pragma HLS PIPELINE
+    symbols[s + 24] = 0;
+    decoded[s + 24] = 0;
+  }
+
+  // g_function_proba_in_after_rate_0
+  loop_9 : for (int s = 0; s < 4; s++) {
+#pragma HLS PIPELINE
+    lwht_in_a   = internal[s + 240];
+    lwht_in_b   = internal[s + 244];
+    memo_in_a   = datapath(lwht_in_a, lwht_in_b, false, 0);
+    internal[s + 248] = memo_in_a;
+  }
+
+  // middle_node_pruned_spc_after_g
+  loop_10 : for (int s = 0; s < 4; s++) {
+#pragma HLS PIPELINE
+    	uint8_t e = vec_decision( internal[s + 248], false );
+    	symbols[s + 28] = e;
+    	decoded[s + 28] = e;
+    	symbol_x = (s == 0) ? symbol_v : (symbol_x ^ symbol_v);
+  }
+  // SPC processing !!!
+
+  loop_xor_11 : for(int i = 0; i < 4; i += 1){
+    symbols[24 + i] ^= symbols[28 + i];
+  }
+
+  loop_xor_12 : for(int i = 0; i < 8; i += 1){
+    symbols[16 + i] ^= symbols[24 + i];
+  }
+
+  loop_xor_13 : for(int i = 0; i < 16; i += 1){
+    symbols[0 + i] ^= symbols[16 + i];
+  }
+
+  // g_function_freq_in
+  loop_14 : for (int s = 0; s < 32; s++) {
+#pragma HLS PIPELINE
+    	lwht_in_a   = internal[s + 128];
+    	lwht_in_b   = internal[s + 160];
+    	memo_in_a   = datapath(lwht_in_a, lwht_in_b, true, 0);
+    	internal[s + 192] = memo_in_a;
+  }
+
+  // f_function_proba_in
+  loop_15 : for (int s = 0; s < 16; s++) {
+#pragma HLS PIPELINE
+    lwht_in_a   = internal[s + 192];
+    lwht_in_b   = internal[s + 208];
+    memo_in_a   = datapath(lwht_in_a, lwht_in_b, true, 0);
+    internal[s + 224] = memo_in_a;
+  }
+
+  // f_function_freq_in
+  loop_16 : for (int s = 0; s < 8; s++) {
+#pragma HLS PIPELINE
+    lwht_in_a   = internal[s + 224];
+    lwht_in_b   = internal[s + 232];
+    memo_in_a   = datapath(lwht_in_a, lwht_in_b, false, 0);
+    internal[s + 240] = memo_in_a;
+  }
+
+  // f_function_freq_in<64>(....); NO F COMPUTATIONS AS WE HAVE A RATE 0 NODE AFTER !
+  // middle_node_pruned_rate_0
+  loop_17 : for (int s = 0; s < 4; s++) {
+#pragma HLS PIPELINE
+    symbols[s + 32] = 0;
+    decoded[s + 32] = 0;
+  }
+
+  // g_function_freq_in_after_rate_0
+  loop_18 : for (int s = 0; s < 4; s++) {
+#pragma HLS PIPELINE
+    lwht_in_a   = internal[s + 240];
+    lwht_in_b   = internal[s + 244];
+    memo_in_a   = datapath(lwht_in_a, lwht_in_b, true, 0);
+    internal[s + 248] = memo_in_a;
+  }
+
+  // middle_node_pruned_spc_after_g
+  loop_19 : for (int s = 0; s < 4; s++) {
+#pragma HLS PIPELINE
+    	uint8_t e = vec_decision( internal[s + 248], false );
+    	symbols[s + 36] = e;
+    	decoded[s + 36] = e;
+    	symbol_x = (s == 0) ? symbol_v : (symbol_x ^ symbol_v);
+  }
+  // SPC processing !!!
+
+  loop_xor_20 : for(int i = 0; i < 4; i += 1){
+    symbols[32 + i] ^= symbols[36 + i];
+  }
+
+  // g_function_freq_in
+  loop_21 : for (int s = 0; s < 8; s++) {
+#pragma HLS PIPELINE
+    	lwht_in_a   = internal[s + 224];
+    	lwht_in_b   = internal[s + 232];
+    	memo_in_a   = datapath(lwht_in_a, lwht_in_b, true, 0);
+    	internal[s + 240] = memo_in_a;
+  }
+
+  // middle_node_pruned_spc_after_g
+  loop_22 : for (int s = 0; s < 8; s++) {
+#pragma HLS PIPELINE
+    	uint8_t e = vec_decision( internal[s + 240], false );
+    	symbols[s + 40] = e;
+    	decoded[s + 40] = e;
+    	symbol_x = (s == 0) ? symbol_v : (symbol_x ^ symbol_v);
+  }
+  // SPC processing !!!
+
+  loop_xor_23 : for(int i = 0; i < 8; i += 1){
+    symbols[32 + i] ^= symbols[40 + i];
+  }
+
+  // g_function_proba_in
+	loop_24 : for (int s = 0; s < 16; s++) {
+#pragma HLS PIPELINE
+    	lwht_in_a   = internal[s + 192];
+    	lwht_in_b   = internal[s + 208];
+    	memo_in_a   = datapath(lwht_in_a, lwht_in_b, false, 0);
+    	internal[s + 224] = memo_in_a;
+  }
+
+  // middle_node_pruned_spc_after_g
+  loop_25 : for (int s = 0; s < 16; s++) {
+#pragma HLS PIPELINE
+    	uint8_t e = vec_decision( internal[s + 224], false );
+    	symbols[s + 48] = e;
+    	decoded[s + 48] = e;
+    	symbol_x = (s == 0) ? symbol_v : (symbol_x ^ symbol_v);
+  }
+  // SPC processing !!!
+
+  loop_xor_26 : for(int i = 0; i < 16; i += 1){
+    symbols[32 + i] ^= symbols[48 + i];
+  }
+
+  loop_xor_27 : for(int i = 0; i < 32; i += 1){
+    symbols[0 + i] ^= symbols[32 + i];
+  }
+
+  // g_function_freq_in
+  loop_28 : for (int s = 0; s < 64; s++) {
+#pragma HLS PIPELINE
+    	lwht_in_a   = internal[s + 0];
+    	lwht_in_b   = internal[s + 64];
+    	memo_in_a   = datapath(lwht_in_a, lwht_in_b, true, 0);
+    	internal[s + 128] = memo_in_a;
+  }
+
+  // f_function_proba_in
+  loop_29 : for (int s = 0; s < 32; s++) {
+#pragma HLS PIPELINE
+    lwht_in_a   = internal[s + 128];
+    lwht_in_b   = internal[s + 160];
+    memo_in_a   = datapath(lwht_in_a, lwht_in_b, true, 0);
+    internal[s + 192] = memo_in_a;
+  }
+
+  // f_function_freq_in
+  loop_30 : for (int s = 0; s < 16; s++) {
+#pragma HLS PIPELINE
+    lwht_in_a   = internal[s + 192];
+    lwht_in_b   = internal[s + 208];
+    memo_in_a   = datapath(lwht_in_a, lwht_in_b, false, 0);
+    internal[s + 224] = memo_in_a;
+  }
+
+  // f_function_freq_in
+  loop_31 : for (int s = 0; s < 8; s++) {
+#pragma HLS PIPELINE
+    lwht_in_a   = internal[s + 224];
+    lwht_in_b   = internal[s + 232];
+    memo_in_a   = datapath(lwht_in_a, lwht_in_b, false, 0);
+    internal[s + 240] = memo_in_a;
+  }
+
+  // f_function_freq_in
+  loop_32 : for (int s = 0; s < 4; s++) {
+#pragma HLS PIPELINE
+    lwht_in_a   = internal[s + 240];
+    lwht_in_b   = internal[s + 244];
+    memo_in_a   = datapath(lwht_in_a, lwht_in_b, false, 0);
+    internal[s + 248] = memo_in_a;
+  }
+
+  //middle_node_pruned_rep_after_f<gf_size>(internal + 248, decoded + 64, symbols + 64, 4);
+  // middle_node_pruned_rep_after_f
+  loop_33 : for (int s = 0; s < 2; s++) {
+#pragma HLS PIPELINE
+    lwht_in_a   = internal[s + 248];
+    lwht_in_b   = internal[s + 250];
+    memo_in_a   = datapath(lwht_in_a, lwht_in_b, true, 0);
+    internal[s + 252] = memo_in_a;
+  }
+  lwht_in_a   = internal[252];
+  lwht_in_b   = internal[253];
+  memo_in_a   = datapath(lwht_in_a, lwht_in_b, false, 0);
+  internal[254] = memo_in_a;
+  symbol_v = vec_decision( internal[254], false );
+  loop_34 : for (int s = 0; s < 4; s++) {
+    symbols[s + 64] = symbol_v;
+    decoded[s + 64] = symbol_v;
+  }
+
+  // g_function_freq_in
+  loop_35 : for (int s = 0; s < 4; s++) {
+#pragma HLS PIPELINE
+    	lwht_in_a   = internal[s + 240];
+    	lwht_in_b   = internal[s + 244];
+    	memo_in_a   = datapath(lwht_in_a, lwht_in_b, true, 0);
+    	internal[s + 248] = memo_in_a;
+  }
+
+  // middle_node_pruned_spc_after_g
+  loop_36 : for (int s = 0; s < 4; s++) {
+#pragma HLS PIPELINE
+    	uint8_t e = vec_decision( internal[s + 248], false );
+    	symbols[s + 68] = e;
+    	decoded[s + 68] = e;
+    	symbol_x = (s == 0) ? symbol_v : (symbol_x ^ symbol_v);
+  }
+  // SPC processing !!!
+
+  loop_xor_37 : for(int i = 0; i < 4; i += 1){
+    symbols[64 + i] ^= symbols[68 + i];
+  }
+
+  // g_function_freq_in
+  loop_38 : for (int s = 0; s < 8; s++) {
+#pragma HLS PIPELINE
+    	lwht_in_a   = internal[s + 224];
+    	lwht_in_b   = internal[s + 232];
+    	memo_in_a   = datapath(lwht_in_a, lwht_in_b, true, 0);
+    	internal[s + 240] = memo_in_a;
+  }
+
+  // middle_node_pruned_spc_after_g
+  loop_39 : for (int s = 0; s < 8; s++) {
+#pragma HLS PIPELINE
+    	uint8_t e = vec_decision( internal[s + 240], false );
+    	symbols[s + 72] = e;
+    	decoded[s + 72] = e;
+    	symbol_x = (s == 0) ? symbol_v : (symbol_x ^ symbol_v);
+  }
+  // SPC processing !!!
+
+  loop_xor_40 : for(int i = 0; i < 8; i += 1){
+    symbols[64 + i] ^= symbols[72 + i];
+  }
+
+  // g_function_freq_in
+  loop_41 : for (int s = 0; s < 16; s++) {
+#pragma HLS PIPELINE
+    	lwht_in_a   = internal[s + 192];
+    	lwht_in_b   = internal[s + 208];
+    	memo_in_a   = datapath(lwht_in_a, lwht_in_b, true, 0);
+    	internal[s + 224] = memo_in_a;
+  }
+
+  // middle_node_pruned_spc_after_g
+  loop_42 : for (int s = 0; s < 16; s++) {
+#pragma HLS PIPELINE
+    	uint8_t e = vec_decision( internal[s + 224], false );
+    	symbols[s + 80] = e;
+    	decoded[s + 80] = e;
+    	symbol_x = (s == 0) ? symbol_v : (symbol_x ^ symbol_v);
+  }
+  // SPC processing !!!
+
+  loop_xor_43 : for(int i = 0; i < 16; i += 1){
+    symbols[64 + i] ^= symbols[80 + i];
+  }
+
+  // g_function_proba_in
+	loop_44 : for (int s = 0; s < 32; s++) {
+#pragma HLS PIPELINE
+    	lwht_in_a   = internal[s + 128];
+    	lwht_in_b   = internal[s + 160];
+    	memo_in_a   = datapath(lwht_in_a, lwht_in_b, false, 0);
+    	internal[s + 192] = memo_in_a;
+  }
+
+  // middle_node_pruned_spc_after_g
+  loop_45 : for (int s = 0; s < 32; s++) {
+#pragma HLS PIPELINE
+    	uint8_t e = vec_decision( internal[s + 192], false );
+    	symbols[s + 96] = e;
+    	decoded[s + 96] = e;
+    	symbol_x = (s == 0) ? symbol_v : (symbol_x ^ symbol_v);
+  }
+  // SPC processing !!!
+
+  loop_xor_46 : for(int i = 0; i < 32; i += 1){
+    symbols[64 + i] ^= symbols[96 + i];
+  }
+
+  loop_xor_47 : for(int i = 0; i < 64; i += 1){
+    symbols[0 + i] ^= symbols[64 + i];
+  }
+
+  // g_function_proba_in
+  loop_48 : for (int s = 0; s < 128; s++) {
+#pragma HLS PIPELINE
+    lwht_in_a   = channel[s    ];
+    lwht_in_b   = channel[s + 128];
+    memo_in_a   = datapath(lwht_in_a, lwht_in_b, false, 0);
+    internal[s] = memo_in_a;
+  }
+
+  // f_function_proba_in
+  loop_49 : for (int s = 0; s < 64; s++) {
+#pragma HLS PIPELINE
+    lwht_in_a   = internal[s + 0];
+    lwht_in_b   = internal[s + 64];
+    memo_in_a   = datapath(lwht_in_a, lwht_in_b, true, 0);
+    internal[s + 128] = memo_in_a;
+  }
+
+  // f_function_freq_in
+  loop_50 : for (int s = 0; s < 32; s++) {
+#pragma HLS PIPELINE
+    lwht_in_a   = internal[s + 128];
+    lwht_in_b   = internal[s + 160];
+    memo_in_a   = datapath(lwht_in_a, lwht_in_b, false, 0);
+    internal[s + 192] = memo_in_a;
+  }
+
+  // f_function_freq_in
+  loop_51 : for (int s = 0; s < 16; s++) {
+#pragma HLS PIPELINE
+    lwht_in_a   = internal[s + 192];
+    lwht_in_b   = internal[s + 208];
+    memo_in_a   = datapath(lwht_in_a, lwht_in_b, false, 0);
+    internal[s + 224] = memo_in_a;
+  }
+
+  // f_function_freq_in
+  loop_52 : for (int s = 0; s < 8; s++) {
+#pragma HLS PIPELINE
+    lwht_in_a   = internal[s + 224];
+    lwht_in_b   = internal[s + 232];
+    memo_in_a   = datapath(lwht_in_a, lwht_in_b, false, 0);
+    internal[s + 240] = memo_in_a;
+  }
+
+  // f_function_freq_in
+  loop_53 : for (int s = 0; s < 4; s++) {
+#pragma HLS PIPELINE
+    lwht_in_a   = internal[s + 240];
+    lwht_in_b   = internal[s + 244];
+    memo_in_a   = datapath(lwht_in_a, lwht_in_b, false, 0);
+    internal[s + 248] = memo_in_a;
+  }
+
+  //middle_node_pruned_rep_after_f<gf_size>(internal + 248, decoded + 128, symbols + 128, 4);
+  // middle_node_pruned_rep_after_f
+  loop_54 : for (int s = 0; s < 2; s++) {
+#pragma HLS PIPELINE
+    lwht_in_a   = internal[s + 248];
+    lwht_in_b   = internal[s + 250];
+    memo_in_a   = datapath(lwht_in_a, lwht_in_b, true, 0);
+    internal[s + 252] = memo_in_a;
+  }
+  lwht_in_a   = internal[252];
+  lwht_in_b   = internal[253];
+  memo_in_a   = datapath(lwht_in_a, lwht_in_b, false, 0);
+  internal[254] = memo_in_a;
+  symbol_v = vec_decision( internal[254], false );
+  loop_55 : for (int s = 0; s < 4; s++) {
+    symbols[s + 128] = symbol_v;
+    decoded[s + 128] = symbol_v;
+  }
+
+  // g_function_freq_in
+  loop_56 : for (int s = 0; s < 4; s++) {
+#pragma HLS PIPELINE
+    	lwht_in_a   = internal[s + 240];
+    	lwht_in_b   = internal[s + 244];
+    	memo_in_a   = datapath(lwht_in_a, lwht_in_b, true, 0);
+    	internal[s + 248] = memo_in_a;
+  }
+
+  // middle_node_pruned_spc_after_g
+  loop_57 : for (int s = 0; s < 4; s++) {
+#pragma HLS PIPELINE
+    	uint8_t e = vec_decision( internal[s + 248], false );
+    	symbols[s + 132] = e;
+    	decoded[s + 132] = e;
+    	symbol_x = (s == 0) ? symbol_v : (symbol_x ^ symbol_v);
+  }
+  // SPC processing !!!
+
+  loop_xor_58 : for(int i = 0; i < 4; i += 1){
+    symbols[128 + i] ^= symbols[132 + i];
+  }
+
+  // g_function_freq_in
+  loop_59 : for (int s = 0; s < 8; s++) {
+#pragma HLS PIPELINE
+    	lwht_in_a   = internal[s + 224];
+    	lwht_in_b   = internal[s + 232];
+    	memo_in_a   = datapath(lwht_in_a, lwht_in_b, true, 0);
+    	internal[s + 240] = memo_in_a;
+  }
+
+  // middle_node_pruned_spc_after_g
+  loop_60 : for (int s = 0; s < 8; s++) {
+#pragma HLS PIPELINE
+    	uint8_t e = vec_decision( internal[s + 240], false );
+    	symbols[s + 136] = e;
+    	decoded[s + 136] = e;
+    	symbol_x = (s == 0) ? symbol_v : (symbol_x ^ symbol_v);
+  }
+  // SPC processing !!!
+
+  loop_xor_61 : for(int i = 0; i < 8; i += 1){
+    symbols[128 + i] ^= symbols[136 + i];
+  }
+
+  // g_function_freq_in
+  loop_62 : for (int s = 0; s < 16; s++) {
+#pragma HLS PIPELINE
+    	lwht_in_a   = internal[s + 192];
+    	lwht_in_b   = internal[s + 208];
+    	memo_in_a   = datapath(lwht_in_a, lwht_in_b, true, 0);
+    	internal[s + 224] = memo_in_a;
+  }
+
+  // middle_node_pruned_spc_after_g
+  loop_63 : for (int s = 0; s < 16; s++) {
+#pragma HLS PIPELINE
+    	uint8_t e = vec_decision( internal[s + 224], false );
+    	symbols[s + 144] = e;
+    	decoded[s + 144] = e;
+    	symbol_x = (s == 0) ? symbol_v : (symbol_x ^ symbol_v);
+  }
+  // SPC processing !!!
+
+  loop_xor_64 : for(int i = 0; i < 16; i += 1){
+    symbols[128 + i] ^= symbols[144 + i];
+  }
+
+  // g_function_freq_in
+  loop_65 : for (int s = 0; s < 32; s++) {
+#pragma HLS PIPELINE
+    	lwht_in_a   = internal[s + 128];
+    	lwht_in_b   = internal[s + 160];
+    	memo_in_a   = datapath(lwht_in_a, lwht_in_b, true, 0);
+    	internal[s + 192] = memo_in_a;
+  }
+
+  // middle_node_pruned_spc_after_g
+  loop_66 : for (int s = 0; s < 32; s++) {
+#pragma HLS PIPELINE
+    	uint8_t e = vec_decision( internal[s + 192], false );
+    	symbols[s + 160] = e;
+    	decoded[s + 160] = e;
+    	symbol_x = (s == 0) ? symbol_v : (symbol_x ^ symbol_v);
+  }
+  // SPC processing !!!
+
+  loop_xor_67 : for(int i = 0; i < 32; i += 1){
+    symbols[128 + i] ^= symbols[160 + i];
+  }
+
+  // g_function_proba_in
+	loop_68 : for (int s = 0; s < 64; s++) {
+#pragma HLS PIPELINE
+    	lwht_in_a   = internal[s + 0];
+    	lwht_in_b   = internal[s + 64];
+    	memo_in_a   = datapath(lwht_in_a, lwht_in_b, false, 0);
+    	internal[s + 128] = memo_in_a;
+  }
+
+  // middle_node_pruned_spc_after_g
+  loop_69 : for (int s = 0; s < 64; s++) {
+#pragma HLS PIPELINE
+    	uint8_t e = vec_decision( internal[s + 128], false );
+    	symbols[s + 192] = e;
+    	decoded[s + 192] = e;
+    	symbol_x = (s == 0) ? symbol_v : (symbol_x ^ symbol_v);
+  }
+  // SPC processing !!!
+
+  loop_xor_70 : for(int i = 0; i < 64; i += 1){
+    symbols[128 + i] ^= symbols[192 + i];
+  }
+
+}
+
